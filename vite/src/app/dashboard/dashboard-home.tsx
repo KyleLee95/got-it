6b19ba5c95ab25223989d39dc353f3b2a08a1fae
kyle <kyle@kylelee.dev>
2025-01-17 15:37:26 -0500

    remove vite

--- vite/src/app/dashboard/dashboard-home.tsx
+++
@@ -1,9 +0,0 @@
-const DashboardHome = () => {
-  return (
-    <div className="flex justify-center">
-      <div>Hello world</div>
-    </div>
-  );
-};
-
-export default DashboardHome;
--- vite/src/app/jobs/jobs.tsx
+++
@@ -1,5 +0,0 @@
-const Jobs = () => {
-  return <div className="bg-red-900">Jobs</div>;
-};
-
-export default Jobs;

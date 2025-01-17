936b414f948e323a98465057107acab73e527bcd
Kyle Lee <kyle@kylelee.dev>
2025-01-16 21:51:05 -0500

    add basic axum server with examples

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,26 +1,176 @@
-use axum::{
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{get, post},
-    Json, Router, ServiceExt,
-};
-
-use serde::{Deserialize, Serialize};
+use axum::routing::get;
+use serde_json::{json, Value};
 use std::net::SocketAddr;
 
-#[tokio::main]
-async fn main() {
-    tracing_subscriber::fmt::init();
-    let app = Router::new().route("/", get(root));
+/// Use Thread for spawning a thread e.g. to acquire our DATA mutex lock.
+use std::thread;
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::info!("listening on {}", addr);
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app.into_make_service())
-        .await
-        .unwrap();
+/// Use tracing crates for application-level tracing output.
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+mod book;
+mod data;
+
+use crate::book::Book;
+use crate::data::DATA;
+
+/// To access data, create a thread, spawn it, then get the lock.
+/// When you're done, then join the thread with its parent thread.
+// async fn print_data() {
+//     thread::spawn(move || {
+//         let data = DATA.lock().unwrap();
+//         println!("data: {:?}", data);
+//     })
+//     .join()
+//     .unwrap()
+// }
+
+/// axum handler for "GET /books/{id}" which responds with one resource HTML page.
+/// This demo app uses our DATA variable, and iterates on it to find the id.
+pub async fn get_books_id(
+    axum::extract::Path(id): axum::extract::Path<u32>,
+) -> axum::response::Html<String> {
+    thread::spawn(move || {
+        let data = DATA.lock().unwrap();
+        match data.get(&id) {
+            Some(book) => format!("<p>{}</p>\n", &book),
+            None => format!("<p>Book id {} not found</p>", id),
+        }
+    })
+    .join()
+    .unwrap()
+    .into()
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+/// axum handler for "GET /books/{id}/form" which responds with a form.
+/// This demo shows how to write a typical HTML form with input fields.
+pub async fn get_books_id_form(
+    axum::extract::Path(id): axum::extract::Path<u32>,
+) -> axum::response::Html<String> {
+    thread::spawn(move || {
+        let data = DATA.lock().unwrap();
+        match data.get(&id) {
+            Some(book) => format!(
+                concat!(
+                    "<form method=\"post\" action=\"/books/{}/form\">\n",
+                    "<input type=\"hidden\" name=\"id\" value=\"{}\">\n",
+                    "<p><input name=\"title\" value=\"{}\"></p>\n",
+                    "<p><input name=\"author\" value=\"{}\"></p>\n",
+                    "<input type=\"submit\" value=\"Save\">\n",
+                    "</form>\n"
+                ),
+                &book.id, &book.id, &book.title, &book.author
+            ),
+            None => format!("<p>Book id {} not found</p>", id),
+        }
+    })
+    .join()
+    .unwrap()
+    .into()
+}
+
+/// axum handler for "GET /books" which responds with a resource page.
+/// This demo uses our DATA; a production app could use a database.
+/// This demo must clone the DATA in order to sort items by title.
+pub async fn get_books() -> axum::response::Html<String> {
+    thread::spawn(move || {
+        let data = DATA.lock().unwrap();
+        let mut books = data.values().collect::<Vec<_>>().clone();
+        books.sort_by(|a, b| a.title.cmp(&b.title));
+        books
+            .iter()
+            .map(|&book| format!("<p>{}</p>\n", &book))
+            .collect::<String>()
+    })
+    .join()
+    .unwrap()
+    .into()
+}
+
+/// axum handler for "PUT /books" which creates a new book resource.
+/// This demo shows how axum can extract JSON data into a Book struct.
+pub async fn put_books(
+    axum::extract::Json(book): axum::extract::Json<Book>,
+) -> axum::response::Html<String> {
+    thread::spawn(move || {
+        let mut data = DATA.lock().unwrap();
+        data.insert(book.id, book.clone());
+        format!("Put book: {}", &book)
+    })
+    .join()
+    .unwrap()
+    .into()
+}
+
+/// axum handler for "DELETE /books/{id}" which destroys a resource.
+/// This demo extracts an id, then mutates the book in the DATA store.
+pub async fn delete_books_id(
+    axum::extract::Path(id): axum::extract::Path<u32>,
+) -> axum::response::Html<String> {
+    thread::spawn(move || {
+        let mut data = DATA.lock().unwrap();
+        if data.contains_key(&id) {
+            data.remove(&id);
+            format!("Delete book id: {}", &id)
+        } else {
+            format!("Book id not found: {}", &id)
+        }
+    })
+    .join()
+    .unwrap()
+    .into()
+}
+
+#[tokio::main]
+pub async fn main() {
+    // Start tracing
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    tracing::event!(tracing::Level::INFO, "main");
+
+    // Build our application by creating our router.
+    let app = axum::Router::new()
+        .fallback(fallback)
+        .route("/", get(hello))
+        .route("/books", get(get_books).put(put_books))
+        .route("/books/{id}", get(get_books_id).delete(delete_books_id))
+        .route("/books/{id}/form", get(get_books_id_form))
+        .route("/demo.json", get(get_demo_json).put(put_demo_json));
+
+    let host = [127, 0, 0, 1];
+    let port = 3000;
+    let addr = SocketAddr::from((host, port));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+/// axum handler for any request that fails to match the router routes.
+/// This implementation returns HTTP status code Not Found (404).
+pub async fn fallback(uri: axum::http::Uri) -> impl axum::response::IntoResponse {
+    (
+        axum::http::StatusCode::NOT_FOUND,
+        format!("No route {}", uri),
+    )
+}
+
+/// axum handler for "GET /" which returns a string and causes axum to
+/// immediately respond with status code `200 OK` and with the string.
+pub async fn hello() -> String {
+    "Hello, World!".to_string()
+}
+/// axum handler for "GET /demo.json" which uses `axum::extract::Json`.
+/// This buffers the request body then deserializes it bu using serde.
+/// The `Json` type supports types that implement `serde::Deserialize`.
+pub async fn get_demo_json() -> axum::extract::Json<Value> {
+    json!({"a":"b"}).into()
+}
+
+/// axum handler for "PUT /demo.json" which uses `axum::extract::Json`.
+/// This buffers the request body then deserializes it using serde.
+/// The `Json` type supports types that implement `serde::Deserialize`.
+pub async fn put_demo_json(
+    axum::extract::Json(data): axum::extract::Json<serde_json::Value>,
+) -> String {
+    format!("Put demo JSON data: {:?}", data)
 }
